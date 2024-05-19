d24bab066173e4f82c595267548ed65469cd43b3
anuja-gode <Anuja.Gode@iiitb.ac.in>
2024-05-19 23:40:24 +0530

    ci/cd added with jenkins

--- signLanguage/constant/training_pipeline/__init__.py
+++ signLanguage/constant/training_pipeline/__init__.py
@@ -41,4 +41,4 @@ MODEL_TRAINER_BATCH_SIZE: int = 16
 MODEL PUSHER related constant start with MODEL_PUSHER var name
 """
 BUCKET_NAME = "sign-lang-mlops-2024"
-S3_MODEL_NAME = "best.pt"
+S3_MODEL_NAME = "my_model.pt"
